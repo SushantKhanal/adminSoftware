c82aed23dfcbc112ba329b1d0f4fc350f41e58e7
SushantKhanal <dreamerking114@gmail.com>
2023-02-03 00:11:13 -0600

    firebase service created

---
+++ src/configs/FirebaseConfig.js
@@ -0,0 +1,74 @@
+import assert from "assert";
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
+assert(REACT_APP_FIREBASE_API_KEY, "FIREBASE_API_KEY is required.");
+assert(REACT_APP_FIREBASE_AUTH_DOMAIN, "FIREBASE_AUTH_DOMAIN is required.");
+assert(REACT_APP_FIREBASE_PROJECT_ID, "FIREBASE_PROJECT_ID is required.");
+assert(
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  "FIREBASE_STORAGE_BUCKET is required."
+);
+assert(
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  "FIREBASE_MESSAGING_SENDER_ID is required."
+);
+assert(REACT_APP_FIREBASE_APP_ID, "FIREBASE_APP_ID is required.");
+assert(
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+  "FIREBASE_MEASUREMENT_ID is required."
+);
+
+export const FirebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+//CRUD Operations on firebase database
+// import { db } from "./firebase-config";
+// import {
+//   collection,
+//   getDocs,
+//   addDoc,
+//   updateDoc,
+//   deleteDoc,
+//   doc,
+// } from "firebase/firestore";
+
+// const userCollectionRef = collection(db, "users");
+// //CREATE
+// const createUser = async() => {
+//   await addDoc(userCollectionRef, {
+//     name: "ABC" + new Date(),
+//     age: 100,
+//   });
+//}
+// //READ
+// const getUsers = async () => {
+//   const data = await getDocs(userCollectionRef);
+//   console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+// };
+// //UPDATE
+// const updateUser = async (id, age) => {
+//   const userDoc = doc(db, "users", id);
+//   const newFields = { age: age + 1 };
+//   await updateDoc(userDoc, newFields);
+// };
+// //DELETE
+// const deleteUser = async (id) => {
+//   const userDoc = doc(db, "users", id);
+//   await deleteDoc(userDoc);
+// };
---
+++ src/services/FirebaseService.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+} from "firebase/auth";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+import { FirebaseConfig } from "../configs/FirebaseConfig";
+
+const firebaseApp = initializeApp(FirebaseConfig);
+
+const FirebaseService = {};
+
+FirebaseService.db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+FirebaseService.currentUser = auth.currentUser;
+const googleAuthProvider = new GoogleAuthProvider();
+const facebookAuthProvider = new FacebookAuthProvider();
+
+FirebaseService.signInEmailRequest = async (email, password) => {
+  return await signInWithEmailAndPassword(auth, email, password)
+    .then((user) => user)
+    .catch((err) => err);
+};
+
+FirebaseService.signOutRequest = async () =>
+  await signOut(auth)
+    .then((user) => user)
+    .catch((err) => err);
+
+FirebaseService.signInGoogleRequest = async () =>
+  await signInWithPopup(auth, googleAuthProvider)
+    .then((user) => user)
+    .catch((err) => err);
+
+FirebaseService.signInFacebookRequest = async () =>
+  await signInWithPopup(auth, facebookAuthProvider)
+    .then((user) => user)
+    .catch((err) => err);
+
+FirebaseService.signUpEmailRequest = async (email, password) =>
+  await createUserWithEmailAndPassword(auth, email, password)
+    .then((user) => user)
+    .catch((err) => err);
+
+export default FirebaseService;
