eb9d700e27645b1ce8ca1674f407b555818238aa
SushantKhanal <dreamerking114@gmail.com>
2023-02-15 12:03:06 -0600

    notification component created

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -2,8 +2,19 @@ import React, { Suspense } from "react";
 import { connect } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { Layout, Grid } from "antd";
+import {
+  SIDE_NAV_WIDTH,
+  SIDE_NAV_COLLAPSED_WIDTH,
+  NAV_TYPE_SIDE,
+  NAV_TYPE_TOP,
+  DIR_RTL,
+  DIR_LTR,
+} from "../constants/ThemeConstant";
 
 const AppLayout = ({ children }) => {
+  //HeaderNav
+  //SideNav & MobileNav
+  //Footer
   return <Layout>{children}</Layout>;
 };
 
